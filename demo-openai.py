aee20d6281ab8b44edcc62d7aa9460953bcb8372
DangLe <lehaidangdev@gmail.com>
2025-05-05 11:43:40 +0700

    initial commit

---
+++ demo-openai.py
@@ -0,0 +1,6 @@
+import streamlit as st
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.chains import RetrievalQA
+from langchain.prompts import PromptTemplate
+from langchain.llms import OpenAI
